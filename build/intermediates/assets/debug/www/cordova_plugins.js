fa84ab960762b399f5f237b929a60a8709f6fc26
vishnuvigneshwar <vishnu@adhiparasakthi.in>
2016-11-12 22:39:04 +0530

    UPDATED CODE

---
+++ build/intermediates/assets/debug/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+        "id": "cordova-plugin-x-socialsharing.SocialSharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.0",
+    "cordova-plugin-x-socialsharing": "5.1.3"
+};
+// BOTTOM OF METADATA
+});
